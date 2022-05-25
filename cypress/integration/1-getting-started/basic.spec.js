9d16b2ffebed7a3920f6b41614464e3506fe5d7b
unknown <you@example.com>
2022-05-25 09:37:25 -0300

    Acessar a pagina e realizar os asserts

---
+++ cypress/integration/1-getting-started/basic.spec.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress"/>
+
+describe('Cypress basics', () => {
+   it('Should visit a page and assert title', () => {
+      cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+      
+       cy.title().should('be.equal', 'Campo de Treinamento')
+       .and('contain', 'Campo')
+   })
+
+   it('Should find and interact with an element', () => {
+      cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+
+      cy.get('#buttonSimple').click()
+      cy.get('#buttonSimple').should('have.value', 'Obrigado!')
+   })
+});
