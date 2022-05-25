0a4c72294473b4afe9d797e71d183892db387f69
unknown <you@example.com>
2022-05-25 10:45:28 -0300

    interagindo com elements

--- cypress/integration/1-getting-started/basic.spec.js
+++ cypress/integration/1-getting-started/basic.spec.js
@@ -3,10 +3,16 @@
 describe('Cypress basics', () => {
    it('Should visit a page and assert title', () => {
       cy.visit('https://www.wcaquino.me/cypress/componentes.html')
-      
-       cy.title().should('be.equal', 'Campo de Treinamento')
-       .and('contain', 'Campo')
+
+      cy.title().should('be.equal', 'Campo de Treinamento')
+         .and('contain', 'Campo')
    })
+
+      
+         /* para 
+         cy.pause()
+         .debug() */
+
 
    it('Should find and interact with an element', () => {
       cy.visit('https://www.wcaquino.me/cypress/componentes.html')
---
+++ cypress/integration/1-getting-started/elements.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe('Work with basic elements', () => {
+    it('Text', () => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+        cy.get('span').should('contain', 'Cuidado')
+        cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
+        
+    })
+});
